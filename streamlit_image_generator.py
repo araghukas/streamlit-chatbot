3a2e49638d6cf05b4e949d469aca116093ccedac
Ara Ghukasyan <ara@agnostiq.ai>
2024-05-03 14:24:47 -0400

    update image gen

--- streamlit_image_generator.py
+++ streamlit_image_generator.py
@@ -29,7 +29,7 @@ def make_image(_prompt, address):
 
     address = address.strip()
     headers = {"x-api-key": DEFAULT_API_KEY}
-    json_ = {"prompt": _prompt, "num_inference_steps": 3}
+    json_ = {"prompt": _prompt, "num_inference_steps": 10}
     try:
         response = requests.post(address + "/text-to-image", json=json_, headers=headers)
         response.raise_for_status()
