9df4057b81fd62442dc1c0621073d51f0c275346
Ara Ghukasyan <ara@agnostiq.ai>
2024-06-10 16:00:21 -0400

    update

--- streamlit_chatbot.py
+++ streamlit_chatbot.py
@@ -23,8 +23,8 @@ from the chat history.
 # Defaults
 MEMORY_LENGTH = 50
 MAX_RESPONSE_TOKENS = 275
-DEFAULT_API_ADDRESS = "https://fn.prod.covalent.xyz/1666755e6003e731c2ae670b4"
-DEFAULT_API_TOKEN = "Yj69c_IlcQUNhxqNMiPd_1gRq1bcAiUjSOuSQ1leIza5FRChScGmAXKxxuxhP1eIKNuDLGbgj8wUDLaq-GICRA"
+DEFAULT_API_ADDRESS = "https: // fn.prod.covalent.xyz/166675436003e731c2ae670b0"
+DEFAULT_API_TOKEN = "l2JkC_YdabVVzse2-nzZ5I0H16xFFsf0iIYNRFuTHH48tBDSySrD3MUQEpay2VvtuXvfHspEzyQoKbAHCqCXLQ"
 
 st.set_page_config(
     page_title="Canadian Chatbot: Curtis",
